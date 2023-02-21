6b9ed6fd10ce2aa840531f7f7144b7e4f01c3c7f
unknown <narmada.vaddireddy97@gmail.com>
2023-02-21 21:48:06 +0530

    Added: equality of two lines based on the end points, So that I know when two lines are equal by using equals method

--- LineComputationProblem.java
+++ LineComputationProblem.java
@@ -1,4 +1,4 @@
-public class Uc1 {
+public class Uc2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Co-ordinates of x1 and y1 : ");
@@ -7,7 +7,20 @@ public class Uc1 {
         System.out.println("Enter the Co-ordinates of x2 and y2 : ");
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
-        double len = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of the co-ordinates : " + len + " units");
+        Double len1 = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
+        System.out.println("Enter the Co-ordinates of x3 and y3 : ");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        System.out.println("Enter the Co-ordinates of x4 and y4 : ");
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+        Double len2 = Math.sqrt((x4 - x3) * (x4 - x3)  +  (y4 - y3) * (y4 - y3));
+        System.out.println("Length of the first Line is : " + len1 + " units" );
+        System.out.println("Length of the second Line is : " + len2 + " units" );
+        System.out.println();
+        if(len1.equals(len2))
+            System.out.println("Both Lines are equal ");
+        else
+            System.out.println("Lines are not equal");
     }
 }
